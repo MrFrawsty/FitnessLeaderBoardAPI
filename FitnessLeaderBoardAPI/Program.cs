df078856400907a7e1b5a719b21f489c2e0408d8
MrFrawsty <kennyjoec@gmail.com>
2023-08-22 19:26:26 -0500

    Added allow AnyHeader and AllowAnyMethod to cors

--- FitnessLeaderBoardAPI/Program.cs
+++ FitnessLeaderBoardAPI/Program.cs
@@ -23,7 +23,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: AllowedOrigins,
         policy =>
         {
-            policy.WithOrigins(" http://localhost:5173");
+            policy.WithOrigins(" http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
         });
 });
 
