ed86abc4136d17ce17d5432532bbdbe2d7dc5273
MrFrawsty <kennyjoec@gmail.com>
2023-10-28 09:27:49 -0400

    Added new migrations

---
+++ FitnessLeaderBoardAPI/Migrations/20231027151823_activitywithdatetimeoffset.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessLeaderBoardAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class activitywithdatetimeoffset : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
